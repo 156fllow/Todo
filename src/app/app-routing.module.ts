780a3ac163013bf79f7d19e0eab6c1e2bb48bf8c
白木　将平 <mjoppaida@icloud.com>
2020-05-10 18:07:47 +0900

    header用ページ追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'list/:id',
     loadChildren: () => import('./list/list.module').then( m => m.ListPageModule)
+  },
+  {
+    path: 'header',
+    loadChildren: () => import('./header/header.module').then( m => m.HeaderPageModule)
   }
 ];
 
